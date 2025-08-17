9b0583db821a9f3fcc99849605e6c10468f9cea1
1Sofia1 <92596963+1Sofia1@users.noreply.github.com>
2025-08-17 23:33:47 +0300

    Update revers_chisla.ino

--- rev_chisla.ino
+++ rev_chisla.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
-
+#include <math.h>
+int chislo;
+void setup() 
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int reverse(int chislo)
+  {
+    int rev=0;
+    while(chislo!=0)
+    {
+      rev = rev*10 + chislo%10;
+      chislo = chislo/10;
+      
+    }
+    return (rev);
+  }
 
+void loop() 
+{
+  Serial.print("Введите число:   ");
+  while (Serial.available() == 0) {}
+  chislo = Serial.parseInt();
+  Serial.println(chislo);
+  while (Serial.available() > 0) { Serial.read(); }
+
+  Serial.println(reverse(chislo));
 }
+
+
+
+
